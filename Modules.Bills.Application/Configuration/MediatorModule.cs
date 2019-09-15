e882f5967e92e75d615630dae97faf54222059a4
Jakub Lotko <1elstac1@gmail.com>
2019-09-15 18:39:58 +0200

    Move data access and mediation configuration to separated modules

---
+++ Modules.Bills.Application/Configuration/MediatorModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using BillAppDDD.Modules.Bills.Application.Contracts;
+using MediatR;
+
+namespace BillAppDDD.Modules.Bills.Application.Configuration
+{
+    class MediatorModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterType<Mediator>()
+                .As<IMediator>();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .AsClosedTypesOf(typeof(ICommandHandler<>))
+                .AsImplementedInterfaces()
+                .InstancePerDependency();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .AsClosedTypesOf(typeof(IRequestHandler<,>))
+                .AsImplementedInterfaces()
+                .InstancePerDependency();
+        }
+    }
+}
