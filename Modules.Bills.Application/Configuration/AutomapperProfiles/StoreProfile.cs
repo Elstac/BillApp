bda740d4a564f018d63d3470f576a060558a9273
Jakub Lotko <1elstac1@gmail.com>
2019-09-18 08:47:20 +0200

    Add store mapping profile

---
+++ Modules.Bills.Application/Configuration/AutomapperProfiles/StoreProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BillAppDDD.Modules.Bills.Application.Stores.Dto;
+using BillAppDDD.Modules.Bills.Domain.Stores;
+
+namespace BillAppDDD.Modules.Bills.Application.Configuration.AutomapperProfiles
+{
+    class StoreProfile:Profile
+    {
+        public StoreProfile()
+        {
+            CreateMap<Store, StoreDto>()
+                .ForMember(
+                s => s.Id,
+                opt => opt.MapFrom(e => e.Id)
+                )
+                .ForMember(
+                s => s.Name,
+                opt => opt.MapFrom(e => e.Name)
+                );
+        }
+    }
+}
