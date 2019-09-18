0d71f66b1104dc5dcefaedf5aa533aa10de2b91d
Jakub Lotko <1elstac1@gmail.com>
2019-09-18 08:34:54 +0200

    Add mapping module

---
+++ Modules.Bills.Application/Configuration/MappingModule.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using AutoMapper;
+using BillAppDDD.Modules.Bills.Application.Configuration.AutomapperProfiles;
+using System;
+using System.Linq;
+
+namespace BillAppDDD.Modules.Bills.Application.Configuration
+{
+    class MappingModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            var profiles =
+                from t in typeof(BillProfile).Assembly.GetTypes()
+                where typeof(Profile).IsAssignableFrom(t)
+                select (Profile)Activator.CreateInstance(t);
+
+            builder.Register(ctx => new MapperConfiguration(cfg =>
+            {
+                foreach (var profile in profiles)
+                {
+                    cfg.AddProfile(profile);
+                }
+            }));
+
+            builder.Register(ctx => ctx.Resolve<MapperConfiguration>().CreateMapper()).As<IMapper>();
+        }
+    }
+}
