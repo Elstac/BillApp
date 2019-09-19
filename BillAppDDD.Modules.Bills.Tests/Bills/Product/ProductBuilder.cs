e4e9153467a8f53a44bd14a2920da8ce4eac9a8d
Jakub Lotko <1elstac1@gmail.com>
2019-09-19 12:53:58 +0200

    Base ProductBuilder

---
+++ BillAppDDD.Modules.Bills.Tests/Bills/Product/ProductBuilder.cs
@@ -0,0 +1,27 @@
+﻿using BillAppDDD.Modules.Bills.Domain.Products;
+
+namespace BillAppDDD.Modules.Bills.Tests.Bills.Product
+{
+    internal class ProductBuilder
+    {
+        private string name;
+        private ProductBarcode barcode;
+        private Price price;
+
+        public ProductBuilder()
+        {
+            name = "";
+            barcode = new ProductBarcode { Value = "" };
+            price = new Price { Value = 0 };
+        }
+
+        public Domain.Products.Product  Build()
+        {
+            return new Domain.Products.Product(
+                name,
+                barcode,
+                price,
+                null)
+        }
+    }
+}
