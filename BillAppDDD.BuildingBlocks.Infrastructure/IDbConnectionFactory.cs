badff7307dbaa7a9e8b09f5a00d50a8b579aa229
Jakub Lotko <1elstac1@gmail.com>
2019-09-26 08:31:44 +0200

    Add dbConnectionFactory

---
+++ BillAppDDD.BuildingBlocks.Infrastructure/IDbConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace BillAppDDD.BuildingBlocks.Infrastructure
+{
+    public interface IDbConnectionFactory
+    {
+        IDbConnection GetDbConnection();
+    }
+}
