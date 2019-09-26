4663a8ea742b76d17aa732f5c4c2ad58df362c18
Jakub Lotko <1elstac1@gmail.com>
2019-09-26 08:57:10 +0200

    Change id type to GUID in StoreDto

--- Modules.Bills.Application/Stores/Dto/StoreDto.cs
+++ Modules.Bills.Application/Stores/Dto/StoreDto.cs
@@ -1,11 +1,10 @@
-﻿using BillAppDDD.Modules.Bills.Application.Bills.Dto;
-using System.Collections.Generic;
+﻿using System;
 
 namespace BillAppDDD.Modules.Bills.Application.Stores.Dto
 {
     public class StoreDto
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
     }
 }
