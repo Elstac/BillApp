a1ad6fead25c9252e28f5771ac313553ae59d88c
Jakub Lotko <1elstac1@gmail.com>
2019-09-26 12:18:57 +0200

    Add BillDetailsV class to contain purchases in JSON format to later deserialization

---
+++ Modules.Bills.Application/Bills/GetBillDetails/BillDetailsV.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BillAppDDD.Modules.Bills.Application.Bills.GetBillDetails
+{
+    internal class BillDetailsV
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public Guid StoreId { get; set; }
+        public string StoreName { get; set; }
+        public string PurchasesJSON { get; set; }
+    }
+}
