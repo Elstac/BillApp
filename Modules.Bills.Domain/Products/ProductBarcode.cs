a65a9dd630643d07a01459708f82d2a81fb2b13e
Jakub Lotko <1elstac1@gmail.com>
2019-09-27 07:58:39 +0200

    Add GetRandomBarcode static method to ProductBarcode

--- Modules.Bills.Domain/Products/ProductBarcode.cs
+++ Modules.Bills.Domain/Products/ProductBarcode.cs
@@ -1,12 +1,27 @@
-﻿namespace BillAppDDD.Modules.Bills.Domain.Products
+﻿using System;
+using System.Text;
+
+namespace BillAppDDD.Modules.Bills.Domain.Products
 {
     public class ProductBarcode
     {
+        private const int maxLength = 10;
         public ProductBarcode(string value)
         {
             Value = value;
         }
 
-        public string Value { get; set; }
+        public string Value { get; private set; }
+
+        public static ProductBarcode GetRandomBarcode()
+        {
+            var rng = new Random();
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < maxLength; i++)
+                sb.Append(rng.Next(0, 10).ToString());
+
+            return new ProductBarcode(sb.ToString());
+        }
     }
 }
