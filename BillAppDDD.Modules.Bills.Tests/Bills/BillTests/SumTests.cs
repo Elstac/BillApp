d4689876087079632d7b8d5fee1f29f5d9d2e104
Jakub Lotko <1elstac1@gmail.com>
2019-10-03 15:54:16 +0200

    Remove unused test

--- BillAppDDD.Modules.Bills.Tests/Bills/BillTests/SumTests.cs
+++ BillAppDDD.Modules.Bills.Tests/Bills/BillTests/SumTests.cs
@@ -8,23 +8,5 @@ namespace BillAppDDD.Modules.Bills.Tests.Bills.BillTests
 
     public class SumTests
     {
-        [Fact]
-        public void Return_sum_of_cost_of_all_purchases()
-        {
-            var purchases = new List<Purchase>
-            {
-                new Purchase(null,new DateTime(),0,10),
-                new Purchase(null,new DateTime(),0,15)
-            };
-
-            var bill = new BillBuilder()
-                .WithPurchases(purchases)
-                .Build();
-
-            var summaryCost = bill.GetSum();
-
-            Assert.Equal(25, summaryCost);
-        }
-
     }
 }
